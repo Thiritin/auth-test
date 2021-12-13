589578cce65aebdb61807dcf5309c2bf6ca4ceac
Thiritin <me@thiritin.com>
2021-12-13 10:20:16 +0100

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+export default {
+  // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
+  ssr: false,
+
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  head: {
+    title: 'authtest',
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: '' },
+      { name: 'format-detection', content: 'telephone=no' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  auth: {
+    strategies: {
+      oidc: {
+        scheme: 'openIDConnect',
+        clientId: 'kbyuFDidLLm280LIwVFiazOqjO3ty8KH',
+        endpoints: {
+          configuration: 'https://accounts.google.com/.well-known/openid-configuration',
+        },
+      }
+    }
+  },
+
+  router: {
+    middleware: ['auth']
+  },
+
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [
+  ],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    // https://go.nuxtjs.dev/axios
+    '@nuxtjs/axios',
+    '@nuxtjs/auth-next'
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {
+    // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
+    baseURL: '/',
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
---
+++ pages/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Login</h1>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  mounted() {
+    this.$auth.loginWith('oidc')
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
